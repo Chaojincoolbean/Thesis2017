2952994dbe3a350d9767e28136dd09026dcfffd7
010011100010 <ll3305@nyu.edu>
2017-09-16 19:58:47 -0400

    Do Tween impemetation

---
+++ Endless_Shooter/Endless_Shooter/Assets/Scrips/MP40Fire.cs
@@ -0,0 +1,36 @@
+﻿namespace VRTK.Examples {
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+	public class MP40Fire : VRTK_InteractableObject {
+
+	// Use this for initialization
+		public GameObject muzzleFlash;
+		private Transform slide;
+		private Transform muzzle;
+
+	public override void StartUsing(VRTK_InteractUse usingObject)
+		{
+			base.StartUsing(usingObject);
+			FireRayCast();
+		}
+	protected void Start () {
+			slide = gameObject.transform.GetChild (2);
+			muzzle = gameObject.transform.GetChild (4);
+		}
+	
+	// Update is called once per frame
+		private void FireRayCast () {
+			print ("gun fired");
+			slide.DOLocalMoveY (-43f, 0.07f);
+			Invoke ("SlideRetract", 0.07f);
+			Instantiate (muzzleFlash, muzzle.position, Quaternion.identity);
+		}
+
+		private void SlideRetract() {
+			slide.DOLocalMoveY (3.147339f, 0.07f);
+		}
+	}
+}
