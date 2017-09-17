afe98e1dc0ff3977234f60c6bda763608353a7b3
010011100010 <ll3305@nyu.edu>
2017-09-17 19:05:18 -0400

    Asset update
    Added explosion SFX.

---
+++ Endless_Shooter/Endless_Shooter/Assets/Scrips/targetGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetGenerator : MonoBehaviour {
+	public GameObject targets;
+	public float targetsNumber = 2f;
+	private float targetsHeight;
+	// Use this for initialization
+	void Start () {
+		targetsHeight = GameObject.Find ("Block 1").GetComponent<buildingGenerator> ().buildingHeight;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
