1bfc0bac3ed76f1cda58bcb43539b473005b9872
010011100010 <ll3305@nyu.edu>
2017-09-17 15:57:05 -0400

    Bullet impact VFX will self destroy after 3 seconds now

--- Endless_Shooter/Endless_Shooter/Assets/Scrips/selfDestroy.cs
+++ Endless_Shooter/Endless_Shooter/Assets/Scrips/selfDestroy.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class selfDestroy : MonoBehaviour {
-
+	public float suicideTime = 1f;
 	// Use this for initialization
 	void Start () {
-		Destroy (gameObject, 1f);
+		Destroy (gameObject, suicideTime);
 	}
 	
 	// Update is called once per frame
