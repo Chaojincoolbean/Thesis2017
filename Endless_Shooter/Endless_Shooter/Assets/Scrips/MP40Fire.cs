ed928e9896e78caadbb6cd413e1f641d4e5885ea
010011100010 <ll3305@nyu.edu>
2017-09-16 20:39:13 -0400

    Gun update
    Finally finished the slide retraction and muzzle flash

--- Endless_Shooter/Endless_Shooter/Assets/Scrips/MP40Fire.cs
+++ Endless_Shooter/Endless_Shooter/Assets/Scrips/MP40Fire.cs
@@ -10,6 +10,7 @@ using DG.Tweening;
 		public GameObject muzzleFlash;
 		private Transform slide;
 		private Transform muzzle;
+		LineRenderer line;
 
 	public override void StartUsing(VRTK_InteractUse usingObject)
 		{
@@ -19,18 +20,47 @@ using DG.Tweening;
 	protected void Start () {
 			slide = gameObject.transform.GetChild (2);
 			muzzle = gameObject.transform.GetChild (4);
+			line = gameObject.GetComponent<LineRenderer> ();
+			line.enabled = false;
 		}
 	
 	// Update is called once per frame
 		private void FireRayCast () {
 			print ("gun fired");
+			line.enabled = true;
+			Vector3 pos = muzzle.position;
+
+			Ray beamRay = new Ray (pos, transform.up);
+
+			RaycastHit Hit;
+			line.SetPosition (0, beamRay.origin);
+
+			if (Physics.Raycast(beamRay, out Hit, 100))
+			{
+				line.SetPosition(1, Hit.point);
+				if (Hit.collider.GetComponent<Rigidbody>() != null)
+				{
+					Hit.collider.GetComponent<Rigidbody>().AddExplosionForce(5f, Hit.point, 3f, 2f, ForceMode.Impulse);
+				}
+					
+			}
+			else{
+				line.SetPosition(1, beamRay.GetPoint(100));	
+			} 
+
+			Invoke ("BeamOff", 0.1f);
+
 			slide.DOLocalMoveY (-43f, 0.07f);
 			Invoke ("SlideRetract", 0.07f);
-			Instantiate (muzzleFlash, muzzle.position, Quaternion.identity);
+			Instantiate (muzzleFlash, muzzle.position, muzzle.rotation);
 		}
 
 		private void SlideRetract() {
 			slide.DOLocalMoveY (3.147339f, 0.07f);
+		}
+
+		public void BeamOff() {
+			line.enabled = false;
 		}
 	}
 }
