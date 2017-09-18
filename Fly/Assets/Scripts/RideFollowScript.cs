54e146b577eee1d8a272821e4dc945de0a28cbe7
Chaojin <jinchaoonly@gmail.com>
2017-09-18 17:58:13 -0400

    ride follow script

---
+++ Fly/Assets/Scripts/RideFollowScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RideFollowScript : MonoBehaviour {
+
+	GameObject PlayerCamera;
+	public float RidePositionX;
+	public float RidePositionY;
+	public float RidePositionZ;
+
+	// Use this for initialization
+	void Start () {
+
+		PlayerCamera = GameObject.FindGameObjectWithTag ("MainCamera");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.gameObject.transform.position = new Vector3 (PlayerCamera.transform.position.x + RidePositionX, 
+			RidePositionY, PlayerCamera.transform.position.z + RidePositionZ);
+
+		//this.gameObject.transform.rotation = new Quaternion(this.gameObject.transform.localRotation.x,
+			//PlayerCamera.transform.rotation.
+		
+	}
+}
