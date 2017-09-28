2afb40bd94322755cd4d1232fb38042d97cf285b
010011100010 <ll3305@nyu.edu>
2017-09-28 13:45:42 -0400

    Game over scene , timer and score

--- Endless_Shooter/Endless_Shooter/Assets/Scrips/scoreManager.cs
+++ Endless_Shooter/Endless_Shooter/Assets/Scrips/scoreManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class scoreManager : MonoBehaviour {
 	public float timerMaxValue = 180;
@@ -10,21 +11,37 @@ public class scoreManager : MonoBehaviour {
 
 	private float timeLeft;
 	private Text timerText;
+	//public Text finalScoreText;
+	private scoreManager managerInstance;
 
 	void Awake () {
-		
+		DontDestroyOnLoad (this);
+		if (managerInstance == null) {
+			managerInstance = this;
+		} else {
+			DestroyObject (gameObject);
+		}
 	}
 
 	// Use this for initialization
 	void Start () {
 		timeLeft = Random.Range (timerMinValue, timerMaxValue);
 		timerText = GameObject.Find ("Timer").GetComponent<Text> ();
+		Invoke ("LoadGameOverScene", timeLeft + 0.2f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timeLeft -= Time.deltaTime;
 		timerText.text = "Time Left: " + (Mathf.Round (timeLeft));
-		
+
+
+	}
+
+	public void LoadGameOverScene () {
+		SceneManager.LoadScene (1, LoadSceneMode.Single);
+		/*finalScoreText = GameObject.Find ("Final Score").GetComponent<Text> ();
+		Debug.Log (finalScoreText.name);
+		finalScoreText.text = "Final Score: " + score;*/
 	}
 }
