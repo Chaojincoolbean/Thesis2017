f7e2a5cf66710e470f1f6e9949ea644d3de4082f
010011100010 <ll3305@nyu.edu>
2017-10-22 11:53:46 -0400

    Finished object random move funtion.

---
+++ Endless_Shooter/Endless_Shooter/Assets/Scrips/floatRandomly.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class floatRandomly : MonoBehaviour {
+	public float timeToNextDestination = 3f;
+	public float range = 30f;
+	public float speed = 10f;
+	private Vector3 newDestination;
+	private Rigidbody rb;
+	float timer = 0;
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+		//InvokeRepeating ("TweenTo", 1f, timeToNextDestination);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer+=Time.deltaTime;
+		if (timer > timeToNextDestination) {
+			timer = 0;
+			newDestination = getNewRandomPosition ();
+
+		} else {
+			transform.position = Vector3.MoveTowards (transform.position, newDestination, speed * Time.deltaTime);
+		}
+	}
+
+	Vector3 getNewRandomPosition () {
+		float x = Random.Range(-range, range);
+		float z = Random.Range(-range, range);
+		float y = Random.Range(-range, range);
+
+		Vector3 pos = new Vector3(x, y, z);
+		return pos;
+	}
+
+	/*void TweenTo() {
+		newDestination = getNewRandomPosition();
+		gameObject.transform.DOMove (newDestination, timeToNextDestination - 0.1f, false);
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		DOTween.KillAll ();
+	}*/
+}
