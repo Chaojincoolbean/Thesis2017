70ca7f91f7f7959068a7d0798b2753450f4f0698
Corey <crbertelsen@gmail.com>
2017-10-22 17:55:28 -0400

    A new map
    I made a city maze this time, but the running element is unintentionally weakened.

--- Endless_Shooter/Endless_Shooter/Assets/Scrips/targetGenerator.cs
+++ Endless_Shooter/Endless_Shooter/Assets/Scrips/targetGenerator.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class targetGenerator : MonoBehaviour {
-	public GameObject targets;
+	public GameObject[] targets;
 	public float targetsNumber = 2f;
-	public float targetMinHeight = 4f;
+	public float targetMinHeight = 5f;
 	private float targetsHeight;
 	private float lotDimensionX;
 	private float lotDimensionZ;
@@ -14,7 +14,7 @@ public class targetGenerator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		targetsHeight = GameObject.Find ("Block 1").GetComponent<buildingGenerator> ().buildingHeight;
+		targetsHeight = GameObject.Find ("Boundry1").GetComponent<buildingGenerator> ().buildingHeight;
 		lotDimensionX = gameObject.transform.localScale.x;
 		lotDimensionZ = gameObject.transform.localScale.z;
 		lotPosX = gameObject.transform.position.x;
@@ -27,7 +27,7 @@ public class targetGenerator : MonoBehaviour {
 			float targetPosY = Random.Range (targetMinHeight, targetsHeight);
 			float targetLocalScale = Random.Range (5f, 20f);
 
-			GameObject generatedTargets = Instantiate (targets, gameObject.transform.position, Quaternion.identity);
+			GameObject generatedTargets = Instantiate (targets[Random.Range(0, targets.Length)], gameObject.transform.position, Quaternion.identity);
 			generatedTargets.transform.localPosition = new Vector3 (targetPosX, targetPosY, targetPosZ);
 			generatedTargets.transform.localScale = new Vector3 (targetLocalScale, targetLocalScale, targetLocalScale);
 			//generatedTargets.transform.parent = gameObject.transform;
