efdce49c6f6498b4bfc6f0b516fce4bc6025cc02
010011100010 <ll3305@nyu.edu>
2017-10-25 15:44:53 -0400

    Hand made map is almost finshied.

---
+++ Endless_Shooter/Endless_Shooter/Assets/Scrips/boundryExtension/boundryExtensionByDirection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boundryExtensionByDirection : MonoBehaviour {
+	public GameObject wall;
+	public int wallCount = 1;
+	public float x = 25f;
+	public float y = 25f;
+	public float z = 25f;
+	// Use this for initialization
+	void Start () {
+		//int length = 5;
+		Vector3 pos = new Vector3(x,y,z);
+		for (int i = 1; i <= wallCount; i++) {
+			Instantiate (wall, gameObject.transform.localPosition+pos, Quaternion.identity);
+			//length += 1;
+			pos += new Vector3(x,y,z);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
