3f8dadfb5f1fb6c84fab19aaab1696c119c43ab2
010011100010 <ll3305@nyu.edu>
2017-11-17 15:35:21 -0500

    I do not fucking know what I am doing.

---
+++ Endless_Shooter/Endless_Shooter/Assets/Scrips/cameraDepth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraDepth : MonoBehaviour {
+    Camera cam;
+	// Use this for initialization
+	void Start () {
+        cam = gameObject.GetComponent<Camera>();
+        cam.depthTextureMode = DepthTextureMode.DepthNormals;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
