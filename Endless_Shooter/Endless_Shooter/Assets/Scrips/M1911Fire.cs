36ea67f5e9714be9a6dae483ae24f8c655e7b448
Democracy <ll3305@nyu.edu>
2017-11-20 08:35:03 -0500

    Asset update
    Added WW2 and modern low poly weapons

---
+++ Endless_Shooter/Endless_Shooter/Assets/Scrips/M1911Fire.cs
@@ -0,0 +1,21 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using System.Collections;
+
+    public class M1911Fire : VRTK_InteractableObject
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
