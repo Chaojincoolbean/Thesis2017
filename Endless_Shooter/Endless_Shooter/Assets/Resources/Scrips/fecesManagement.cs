7c04764782ccdbfee19db544760e6d292b6cea5e
Democracy <ll3305@nyu.edu>
2017-12-04 15:28:31 -0500

    Fecal update
    Somewhat finished

--- Endless_Shooter/Endless_Shooter/Assets/Resources/Scrips/fecesManagement.cs
+++ Endless_Shooter/Endless_Shooter/Assets/Resources/Scrips/fecesManagement.cs
@@ -10,8 +10,12 @@ public class fecesManagement : MonoBehaviour {
     public Camera cam;
     private int index;
     private GameObject playerBody;
+    private GameObject playArea;
     public GameObject anus;
     public GameObject[] shit;
+    public Material[] gradients;
+    public GameObject asshole;
+    public GameObject butthole;
     float c = 0;
     // Use this for initialization
     void Start () {
@@ -34,6 +38,7 @@ public class fecesManagement : MonoBehaviour {
             Invoke("DisableSlogan", countdown - 1f);
             c = 0f;
         }
+        
 	}
 
     void DisplaySlogan(int i)
@@ -46,7 +51,13 @@ public class fecesManagement : MonoBehaviour {
         c.a = 1;
         slogans[i].GetComponent<Text>().DOColor(c, 1f);
 
+        /*for (int t=0; t<=10; t++)
+        {
+            GameObject shits = Instantiate(shit[Random.Range(0, shit.Length-1)], playArea.transform.position + new Vector3(30, 30, 30), Quaternion.identity) as GameObject;
+            shits.GetComponent<Renderer>().material = gradients[Random.Range(0, gradients.Length-1)];
 
+            Destroy(shits, 5f);
+        }*/
     }
 
     void DisableSlogan()
@@ -57,8 +68,11 @@ public class fecesManagement : MonoBehaviour {
     void FindPlayer()
     {
         playerBody = GameObject.Find("[VRTK][AUTOGEN][BodyColliderContainer]");
+        playArea = GameObject.Find("[VRTK_SDKManager]");
         print(anus);
         anus.transform.parent = playerBody.transform;
+        asshole.transform.parent = playerBody.transform;
+        butthole.transform.parent = playerBody.transform;
         anus.SetActive(true);
     }
 }
