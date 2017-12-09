fd49a1f0e1d1095d60b741e09a9b963d5c7670f9
Game Center VR Cart <ll3305@nyu.edu>
2017-12-09 14:07:00 -0800

    Major update
    A new scene is built and has lighting baked.
    Added AK47. It can be holded by two hands but lacks recoil.

--- Endless_Shooter/Endless_Shooter/Assets/Resources/Scrips/boundryExtension/boundryExtensionByDirection.cs
+++ Endless_Shooter/Endless_Shooter/Assets/Resources/Scrips/boundryExtension/boundryExtensionByDirection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class boundryExtensionByDirection : MonoBehaviour {
 	public GameObject wall;
 	public int wallCount = 1;
@@ -21,6 +22,14 @@ public class boundryExtensionByDirection : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Vector3 pos = new Vector3(x, y, z);
+            for (int i = 1; i <= wallCount; i++)
+            {
+                Instantiate(wall, gameObject.transform.localPosition + pos, Quaternion.identity);
+                //length += 1;
+                pos += new Vector3(x, y, z);
+            }
+        }
+    }
 }
