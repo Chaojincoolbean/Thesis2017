9701333dbc893e6cf7ee48a0cdf931c8ff1f6fdb
010011100010 <ll3305@nyu.edu>
2018-02-08 00:25:54 -0500

    Asset update
    Added AnimFollow asset

---
+++ Endless_Shooter/Endless_Shooter/Assets/Resources/AF_SFIK/AnimFollow/Scripts/SimpleFootIKScripts_AF/IK_PositionFeet_AF.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AnimFollow
+{
+	public partial class SimpleFootIK_AF
+	{
+		void PositionFeet()
+		{
+			// Save before PositionFeet
+			leftFootRotation = leftFoot.rotation;
+			rightFootRotation = rightFoot.rotation;
+
+			// Here goes the maths			
+			leftFootTargetNormal = Vector3.Lerp(Vector3.up, raycastHitLeftFoot.normal, footIKWeight);
+			leftFootTargetNormal = Vector3.Lerp(lastLeftFootTargetNormal, leftFootTargetNormal, footNormalLerp * deltaTime);
+			lastLeftFootTargetNormal = leftFootTargetNormal;
+			rightFootTargetNormal = Vector3.Lerp(Vector3.up, raycastHitRightFoot.normal, footIKWeight);
+			rightFootTargetNormal = Vector3.Lerp(lastRightFootTargetNormal, rightFootTargetNormal, footNormalLerp * deltaTime);
+			lastRightFootTargetNormal = rightFootTargetNormal;
+
+			leftFootTargetPos = raycastHitLeftFoot.point + leftFootTargetNormal * footHeight + (leftFoot.position.y - footHeight - transform.position.y) * Vector3.up;
+			leftFootTargetPos = Vector3.Lerp(leftFoot.position, leftFootTargetPos, footIKWeight);
+			leftFootTargetPos = Vector3.Lerp(lastLeftFootTargetPos, leftFootTargetPos, footTargetLerp * deltaTime);
+			lastLeftFootTargetPos = leftFootTargetPos;
+			
+			rightFootTargetPos = raycastHitRightFoot.point + rightFootTargetNormal * footHeight + (rightFoot.position.y - footHeight - transform.position.y) * Vector3.up;
+			rightFootTargetPos = Vector3.Lerp(rightFoot.position, rightFootTargetPos, footIKWeight);
+			rightFootTargetPos = Vector3.Lerp(lastRightFootTargetPos, rightFootTargetPos, footTargetLerp * deltaTime);
+			lastRightFootTargetPos = rightFootTargetPos;
+
+
+			leftLegTargetLength = Mathf.Min((leftFootTargetPos - leftThigh.position).magnitude, calfLength + thighLength - .01f);
+			leftLegTargetLength = Mathf.Max(leftLegTargetLength, .2f);
+			leftKneeAngle = Mathf.Acos((Mathf.Pow(leftLegTargetLength, 2f) - calfLengthSquared - thighLengthSquared) * reciDenominator);
+			leftKneeAngle *= Mathf.Rad2Deg;
+			leftCalf.Rotate(0f, 0f, 180f - leftKneeAngle - leftCalf.localEulerAngles.z);
+			leftThigh.rotation = Quaternion.FromToRotation(leftFoot.position - leftThigh.position, leftFootTargetPos - leftThigh.position) * leftThigh.rotation;
+
+			rightLegTargetLength = Mathf.Min((rightFootTargetPos - rightThigh.position).magnitude, calfLength + thighLength - .01f);
+			rightLegTargetLength = Mathf.Max(rightLegTargetLength, .2f);
+			rightKneeAngle = Mathf.Acos((Mathf.Pow(rightLegTargetLength, 2f) - calfLengthSquared - thighLengthSquared) * reciDenominator);
+			rightKneeAngle *= Mathf.Rad2Deg;
+			rightCalf.Rotate(0f, 0f, 180f - rightKneeAngle - rightCalf.localEulerAngles.z);
+			rightThigh.rotation = Quaternion.FromToRotation(rightFoot.position - rightThigh.position, rightFootTargetPos - rightThigh.position) * rightThigh.rotation;
+			
+			leftFoot.rotation = Quaternion.FromToRotation(Vector3.up, leftFootTargetNormal) * leftFootRotation;
+			rightFoot.rotation = Quaternion.FromToRotation(Vector3.up, rightFootTargetNormal) * rightFootRotation;
+		}
+	}
+}
---
+++ Endless_Shooter/Endless_Shooter/Assets/Resources/AF_SFIK/AnimFollow/Scripts/SimpleFootIKScripts_AF/IK_ShootRays_AF.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AnimFollow
+{
+	public partial class SimpleFootIK_AF
+	{
+		void ShootIKRays()
+		{		
+			// Shoot ray to determine where the feet should be placed.
+			if (!Physics.Raycast(rightFoot.position + Vector3.up * maxStepHeight, Vector3.down, out raycastHitRightFoot, raycastLength, layerMask))
+			{
+				if (!Physics.Raycast(rightFoot.position + Vector3.up * 2f, Vector3.down, out raycastHitRightFoot, raycastLength * 4f, layerMask))
+				{
+					raycastHitRightFoot.normal = Vector3.up;
+					raycastHitRightFoot.point = new Vector3(rightFoot.position.x, transform.position.y - .01f, rightFoot.position.z);
+				}
+			}
+			footForward = rightToe.position - rightFoot.position;
+			footForward = new Vector3(footForward.x, 0f, footForward.z);
+			footForward = Quaternion.FromToRotation(Vector3.up, raycastHitRightFoot.normal) * footForward;
+			if (!Physics.Raycast(rightFoot.position + footForward + Vector3.up * maxStepHeight, Vector3.down, out raycastHitToe, maxStepHeight * 2f, layerMask))
+			{
+				raycastHitToe.normal = raycastHitRightFoot.normal;
+				raycastHitToe.point = raycastHitRightFoot.point + footForward;
+			}
+			else
+			{		
+				if(raycastHitRightFoot.point.y < raycastHitToe.point.y - footForward.y)
+					raycastHitRightFoot.point = new Vector3(raycastHitRightFoot.point.x, raycastHitToe.point.y - footForward.y, raycastHitRightFoot.point.z);
+				
+				// Put avgNormal in foot normal
+				raycastHitRightFoot.normal = (raycastHitRightFoot.normal + raycastHitToe.normal).normalized;
+			}
+			
+			if (!Physics.Raycast(leftFoot.position + Vector3.up * maxStepHeight, Vector3.down, out raycastHitLeftFoot, raycastLength, layerMask))
+			{
+				if (!Physics.Raycast(leftFoot.position + Vector3.up * 2f, Vector3.down, out raycastHitLeftFoot, raycastLength * 4f, layerMask))
+				{
+					raycastHitLeftFoot.normal = Vector3.up;		
+					raycastHitLeftFoot.point = new Vector3(leftFoot.position.x, transform.position.y - .01f, leftFoot.position.z);
+				}
+			}
+			footForward = leftToe.position - leftFoot.position;
+			footForward = new Vector3(footForward.x, 0f, footForward.z);
+			footForward = Quaternion.FromToRotation(Vector3.up, raycastHitLeftFoot.normal) * footForward;
+			if (!Physics.Raycast(leftFoot.position + footForward + Vector3.up * maxStepHeight, Vector3.down, out raycastHitToe, maxStepHeight * 2f, layerMask))
+			{
+				raycastHitToe.normal = raycastHitLeftFoot.normal;
+				raycastHitToe.point = raycastHitLeftFoot.point + footForward;
+			}
+			else
+			{
+				if(raycastHitLeftFoot.point.y < raycastHitToe.point.y - footForward.y)
+					raycastHitLeftFoot.point = new Vector3(raycastHitLeftFoot.point.x, raycastHitToe.point.y - footForward.y, raycastHitLeftFoot.point.z);
+
+				// Put avgNormal in foot normal
+				raycastHitLeftFoot.normal = (raycastHitLeftFoot.normal + raycastHitToe.normal).normalized;
+			}
+
+			// Do not tilt feet if on to steep an angle
+			if (Vector3.Dot(raycastHitLeftFoot.normal, Vector3.up) < 1f - maxIncline)
+			{
+				raycastHitLeftFoot.normal = Vector3.RotateTowards(Vector3.up, raycastHitLeftFoot.normal, Mathf.Asin(maxIncline), 0f);
+			}
+			if (Vector3.Dot(raycastHitRightFoot.normal, Vector3.up) < 1f - maxIncline)
+			{
+				raycastHitRightFoot.normal = Vector3.RotateTowards(Vector3.up, raycastHitRightFoot.normal, Mathf.Asin(maxIncline), 0f);
+			}
+
+			if (followTerrain)
+			{
+				transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, Mathf.Min(raycastHitLeftFoot.point.y, raycastHitRightFoot.point.y), transformYLerp * extraYLerp * deltaTime), transform.position.z);
+			}
+		}
+	}
+}
