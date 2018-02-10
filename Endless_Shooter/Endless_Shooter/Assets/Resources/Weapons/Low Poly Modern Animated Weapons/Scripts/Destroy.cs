82f99bd80abbbff397cbffd10961d54be457eeae
010011100010 <ll3305@nyu.edu>
2018-02-10 18:24:12 -0500

    Lighting update
    Improved the post processing filther on VR camera;
    Improved light maps on some maps;
    imported more weapons.

---
+++ Endless_Shooter/Endless_Shooter/Assets/Resources/Weapons/Low Poly Modern Animated Weapons/Scripts/Destroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+
+	public float 		timer = 15f;
+	public float 		timer2  = 3f;		
+
+
+	public void Start()
+	{
+		Destroy(gameObject, timer);
+	}
+
+	
+	public void OnCollisionEnter(Collision col) 
+	{		
+		Destroy (gameObject, timer2);
+	}
+
+
+}
