5a0ff8f0f47fa409b98c37f6b22b8147b62537f5
010011100010 <ll3305@nyu.edu>
2018-03-14 18:43:54 -0400

    Updates
    Working on rocket launcher script;
    Added rocket flame trail particle;
    Deleted realistic FPS particle assets.

--- Endless_Shooter/Endless_Shooter/Assets/Resources/Scrips/rocketLauncherFire.cs
+++ Endless_Shooter/Endless_Shooter/Assets/Resources/Scrips/rocketLauncherFire.cs
@@ -6,14 +6,39 @@ using RootMotion.Dynamics;
 
 public class rocketLauncherFire : VRTK_InteractableObject
 {
+    public AudioClip[] grabClips;
+    public AudioClip[] explosionClips;
+    public AudioClip launcherClip;
+    public GameObject[] rockets;
+    public Vector3 rocketStartPos;
 
-	// Use this for initialization
-	void Start () {
-		
+    private VRTK_ControllerEvents controllerEvents;
+    private AudioSource launcherSource;
+
+    public override void Grabbed(VRTK_InteractGrab currentGrabbingObject)
+    {
+        base.Grabbed(currentGrabbingObject);
+        controllerEvents = currentGrabbingObject.GetComponent<VRTK_ControllerEvents>();
+        launcherSource.clip = grabClips[Random.Range(0, grabClips.Length)];
+        launcherSource.Play();
+        if (GetComponent<floatAndSpin>() != null)
+        {
+            GetComponent<floatAndSpin>().enabled = false;
+        }
+    }
+
+    public override void Ungrabbed(VRTK_InteractGrab previousGrabbingObject)
+    {
+        base.Ungrabbed(previousGrabbingObject);
+        controllerEvents = null;
+        previousGrabbingObject.GetComponent<Rigidbody>().useGravity = enabled;
+        previousGrabbingObject.GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    // Use this for initialization
+    void Start () {
+        launcherSource = GetComponent<AudioSource>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
