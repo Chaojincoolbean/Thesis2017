3c54ca21c04eff2bd27c7efb148b17f5ff33d3ff
010011100010 <ll3305@nyu.edu>
2018-03-21 00:05:18 -0400

    Working on a new level

---
+++ Endless_Shooter/Endless_Shooter/Assets/keepRotating.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keepRotating : MonoBehaviour {
+    public float YAxisRotatingSpeed = 100f;
+    public float XAxisRotatingSpeed = 25f;
+    public float ZAxisRotatingSpeed = 75f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * Time.deltaTime * YAxisRotatingSpeed);
+        transform.Rotate(Vector3.right * Time.deltaTime * XAxisRotatingSpeed);
+        transform.Rotate(Vector3.forward * Time.deltaTime * ZAxisRotatingSpeed);
+    }
+}
