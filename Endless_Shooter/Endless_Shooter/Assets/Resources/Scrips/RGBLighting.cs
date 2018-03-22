725ebcc6bc676ba542bfd95aadd69f9f35e6ebc1
010011100010 <ll3305@nyu.edu>
2018-03-21 22:08:48 -0400

    One more enemy
    Mannequin_Mage_Medium_WithLight_RainbowDancer
    This enemy has a RGB emblem on its head that changes color over time. It shoots rainbow colored projectiles.

---
+++ Endless_Shooter/Endless_Shooter/Assets/Resources/Scrips/RGBLighting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RGBLighting : MonoBehaviour {
+    MeshRenderer meshRenderer;
+    float f = 0;
+    // Use this for initialization
+    void Start () {
+        meshRenderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        f += Time.deltaTime;
+        if (f >= 2.1f)
+        {
+            ChangeColor();
+            f = 0;
+        }
+	}
+
+    void ChangeColor()
+    {
+        Color randomColor = (Random.ColorHSV(0f, 1f, 0f, 1f, 0f, 1f)*2);
+        meshRenderer.material.DOColor(randomColor, "_EmissionColor", 2);
+    }
+}
