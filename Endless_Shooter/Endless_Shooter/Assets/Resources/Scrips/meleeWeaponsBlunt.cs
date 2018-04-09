503ef04b80ab95b68ba1c3c67ffc65db32bd2c43
010011100010 <ll3305@nyu.edu>
2018-04-08 22:19:37 -0400

    Funtional updates
    Adjusted the snap handle rotation on some guns;
    Added VR interacble riot shield, shock baton, and lead pipe.

--- Endless_Shooter/Endless_Shooter/Assets/Resources/Scrips/meleeWeaponsBlunt.cs
+++ Endless_Shooter/Endless_Shooter/Assets/Resources/Scrips/meleeWeaponsBlunt.cs
@@ -1,14 +1,20 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using VRTK;
 using RootMotion.Dynamics;
 
-public class meleeWeapons : VRTK_InteractableObject
+public class meleeWeaponsBlunt : VRTK_InteractableObject
 {
-    public MuscleRemoveMode removeMuscleMode;
-    public float damage = 20f;
+    public float damageMultiplier = 1f;
     public float damageTherehold = 20f;
+    //public bool hasSpecialVFX = false;
     VRTK_ControllerEvents controllerEvents;
+    public AudioClip[] bluntClips;
+    public GameObject specialVFX;
+
+    private AudioSource bluntSource;
+
     public override void Grabbed(VRTK_InteractGrab currentGrabbingObject)
     {
         base.Grabbed(currentGrabbingObject);
@@ -22,38 +28,49 @@ public class meleeWeapons : VRTK_InteractableObject
     }
 
     // Use this for initialization
-    void Start()
-    {
-
-    }
+    void Start () {
+        bluntSource = GetComponent<AudioSource>();
+	}
 
     void OnCollisionEnter(Collision collision)
     {
+        //Check if this weapon has a special VFX to spawn one on collision
+        if (specialVFX != null)
+        {
+            ContactPoint contact = collision.contacts[0];
+            Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 pos = contact.point;
+            GameObject VFX = Instantiate(specialVFX, pos, rot);
+            Destroy(VFX, 5f);
+        }
+
         //If whatever stuff collide with this melee weapon has a rigidbody
         if (collision.collider.GetComponent<Rigidbody>() != null)
         {
-            //print(collision.relativeVelocity.magnitude + " From melee blunt");
+            print(collision.relativeVelocity.magnitude + " From melee blunt and hit " + collision.collider.name);
             //If this rigidbody is a puppet muscle
             if (collision.collider.attachedRigidbody.GetComponent<MuscleCollisionBroadcaster>() != null)
             {
+                //Play hit SFX
+                bluntSource.clip = bluntClips[Random.Range(0, bluntClips.Length)];
+                bluntSource.Play();
                 //Damage the mannequin enemy depend on where the player hit and print its remaining health
-                print(collision.collider.name);
-                if(collision.relativeVelocity.magnitude > damageTherehold)
+                //print(collision.collider.name);
+                if (collision.relativeVelocity.magnitude > damageTherehold)
                 {
-                    
+
                     if (collision.collider.name == "Head" || collision.collider.name == "Chest")
                     {
-                        collision.collider.transform.parent.parent.GetChild(2).GetComponent<mannequinBase>().health -= damage * 100f;
-
+                        collision.collider.transform.parent.parent.GetChild(2).GetComponent<mannequinBase>().health -= collision.relativeVelocity.magnitude * damageMultiplier;
                     }
                     else
                     {
-                        collision.collider.transform.parent.parent.GetChild(2).GetComponent<mannequinBase>().health -= damage;
+                        collision.collider.transform.parent.parent.GetChild(2).GetComponent<mannequinBase>().health -= collision.relativeVelocity.magnitude;
                     }
 
                     //Sever the limb
-                    var broadcaster = collision.collider.attachedRigidbody.GetComponent<MuscleCollisionBroadcaster>();
-                    broadcaster.puppetMaster.RemoveMuscleRecursive(broadcaster.puppetMaster.muscles[broadcaster.muscleIndex].joint, true, true, removeMuscleMode);
+                    //var broadcaster = collision.collider.attachedRigidbody.GetComponent<MuscleCollisionBroadcaster>();
+                    //broadcaster.puppetMaster.RemoveMuscleRecursive(broadcaster.puppetMaster.muscles[broadcaster.muscleIndex].joint, true, true, removeMuscleMode);
 
                 }
                 else
@@ -62,7 +79,7 @@ public class meleeWeapons : VRTK_InteractableObject
                 }
 
                 print(collision.collider.transform.parent.parent.GetChild(2).GetComponent<mannequinBase>().health);
-               
+
             }
             else
             {
