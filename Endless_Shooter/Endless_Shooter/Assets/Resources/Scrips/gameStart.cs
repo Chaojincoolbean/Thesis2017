00badc790e6b7a9cd67255cf714c34a72fecdf2d
010011100010 <ll3305@nyu.edu>
2018-05-12 17:54:54 -0400

    Game Start level is almost finished, working on the tutorial

---
+++ Endless_Shooter/Endless_Shooter/Assets/Resources/Scrips/gameStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRTK;
+using VRTK.UnityEventHelper;
+
+
+public class gameStart : MonoBehaviour {
+    private VRTK_Button_UnityEvents buttonEvents;
+    // Use this for initialization
+    void Start () {
+        buttonEvents = GetComponent<VRTK_Button_UnityEvents>();
+        if (buttonEvents == null)
+        {
+            buttonEvents = gameObject.AddComponent<VRTK_Button_UnityEvents>();
+        }
+        buttonEvents.OnPushed.AddListener(handlePush);
+    }
+
+    private void handlePush(object sender, Control3DEventArgs e)
+    {
+        VRTK_Logger.Info("Pushed");
+        print("Pushed");
+        gameObject.GetComponent<AudioSource>().Play();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+    }
+}
